2f669b8bc8ddb8e53a125c1e00c6a50649c57e70
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-01-21 23:39:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def verify_fb_token(token_sent):
 def get_message():
     sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
     # return selected item to the user
-    return 'Deba re deba'
+    return random.choice(sample_responses)
 
 #uses PyMessenger to send response to user
 def send_message(recipient_id, response):
