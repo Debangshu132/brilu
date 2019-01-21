054cff3055b4ae342dc7efe9cab8799806d72822
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-01-21 23:50:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,9 +48,25 @@ def verify_fb_token(token_sent):
 
 #chooses a random message to send to the user
 def get_message():
-    sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
+    #sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
     # return selected item to the user
-    return random.choice(sample_responses)
+   # return random.choice(sample_responses)
+#def singlechat():
+  questions = open("questions.txt", "r+")
+  answers = open("answers.txt", "r+")
+  questionarr=questions.read().split("\n")
+  query=input("How may I help you:")
+  questionarr.append(query)
+  answerarr=answers.read().split("\n")
+  vectorizer=CountVectorizer()
+  ques=vectorizer.fit_transform(questionarr).todense()
+  match=[]
+  for f in range(0,len(ques)-2):
+    p=euclidean_distances(ques[len(ques)-1],ques[f])
+    match.append(p[0][0])
+  m=match.index(min(match))
+  print(answerarr[m])
+  return(answerarr[m])
 
 #uses PyMessenger to send response to user
 def send_message(recipient_id, response):
