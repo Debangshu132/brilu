0bb9112114d0d88aa3f75f5deca09492d37d02ba
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-01-21 23:55:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def receive_message():
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
-                    response_sent_text = get_message()
+                    response_sent_text = message['message'].get('text')
                     send_message(recipient_id, response_sent_text)
                 #if user sends us a GIF, photo,video, or any other non-text item
                 if message['message'].get('attachments'):
