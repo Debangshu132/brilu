a8dee3098e07d557f9d9c7e973f2256ccca9fe67
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-01-28 01:23:41 +0530

    Update decisionTree.py

--- decisionTree.py
+++ decisionTree.py
@@ -1,3 +1,17 @@
+from pymongo import MongoClient
+def fetchData():
+    MONGODB_URI = "mongodb://Debangshu:Starrynight.1@ds163694.mlab.com:63694/brilu"
+    client = MongoClient(MONGODB_URI, connectTimeoutMS=30000)
+    db = client.get_database("brilu")
+    col = db["usersInfo"]
+    cursor = col.find()
+    # p.pprint(cursor[0])
+    document = cursor[0]
+    #p.pprint(document.keys())
+    #p.pprint(document["chitchat"])
+    return(document)
+
+
 def listOfExams():
     return ['JEE Advanced','JEE Mains','GATE']
 def decision(input):
