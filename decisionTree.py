5519d54db74317263a365a1a819cbf02a11efafd
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-01-27 23:43:54 +0530

    Update decisionTree.py

--- decisionTree.py
+++ decisionTree.py
@@ -1,12 +1,15 @@
 def decision(input):
      if input=='JEE Mains':
          msg='Okay so JEE mains it is! I will give you some random questions from mains paper as practice untill you decide its time to do something else'
-         return msg,['Deal','Go Back'] 
+         listitems=['Deal','Go Back']
+         return msg,listitems
      if input=='JEE Advanced':
          msg='Okay so JEE advanced it is! I will give you some random questions from the advanced paper as practice untill you decide its time to do something else'
-         return msg,['Deal','Go Back']  
+         listitems=['Deal','Go Back']
+         return msg,listitems 
      if input=='Go Back':
             msg='okay,Which exam would you like to take'
-            return msg,['JEE Advanced','JEE Mains']
+            listitems=['JEE Advanced','JEE Mains']   
+            return msg, listitems
         
     
