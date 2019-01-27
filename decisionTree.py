b6ed87a05b13e36faaef3320f93be0ca2d38332a
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-01-27 23:33:45 +0530

    Update decisionTree.py

--- decisionTree.py
+++ decisionTree.py
@@ -1,9 +1,12 @@
-def decision(payload):
-    if payload==Startyaar:
-     return (examOptions)
-def examOptions():
-     payload= {"recipient":{"id":id}, 
-                "message":{"text": "Please choose an exam from below:",
-                  "quick_replies":[{"content_type":"text","title":"JEE Advanced","payload":"JEE Advanced"},
-                                   {"content_type":"text","title":"JEE Mains","payload":"JEE Mains"}]}}     
-     return payload    
+def decision(input):
+     if input=='JEE Mains':
+         msg='Okay so JEE mains it is! I will give you some random questions from mains paper as practice untill you decide its time to do something else'
+         return msg,['Deal','Go Back'] 
+     if input=='JEE Advanced':
+         msg='Okay so JEE advanced it is! I will give you some random questions from the advanced paper as practice untill you decide its time to do something else'
+         return msg,['Deal','Go Back']  
+     if input=='Go Back':
+            msg='okay,Which exam would you like to take'
+            return msg,['JEE Advanced','JEE Mains']
+        
+    
