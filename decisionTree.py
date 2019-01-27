ed7505032570fe420e00564cccb2dbcb64490b0a
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-01-27 17:38:20 +0530

    Update decisionTree.py

--- decisionTree.py
+++ decisionTree.py
@@ -1,3 +1,9 @@
 def decision(payload):
     if payload==Startyaar:
      return (examOptions)
+def examOptions():
+     payload= {"recipient":{"id":id}, 
+                "message":{"text": "Please choose an exam from below:",
+                  "quick_replies":[{"content_type":"text","title":"JEE Advanced","payload":"JEE Advanced"},
+                                   {"content_type":"text","title":"JEE Mains","payload":"JEE Mains"}]}}     
+     return payload    
