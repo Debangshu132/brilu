83f89fb822b66df1e1dbc96bdf6930f1575c7bd4
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-01-28 23:48:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,13 @@ def receive_message():
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
                     typingon=pay({"recipient":{"id":recipient_id},"sender_action":"typing_on"})
+                    if  message['message'].get('quick_reply'):
+                      if message['message']['quick_reply']['payload']=='right':
+                        quickreply(recipient_id,['Another one','Go Back'],'Thats right')
+                        return "Message Processed"
+                      if message['message']['quick_reply']['payload']=='wrong':
+                        quickreply(recipient_id,['Try again','Go Back'],'sorry thats wrong!')
+                        return "Message Processed"
                    
                     topic,mood,response = get_message(recipient_id,message['message'].get('text'))
                     #checkPostback(output)
