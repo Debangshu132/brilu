ca1cbc80fedf37865ddf19b56cb59322d201884c
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-01-28 15:50:05 +0530

    Update decisionTree.py

--- decisionTree.py
+++ decisionTree.py
@@ -40,7 +40,7 @@ def decision(input):
             listitems=['Go Back']
             return msg,listitems 
 def askQuestion(topic):
-    question="who is the father of the nation"
+    question=""
     options=['gandhi','nehru','jayanta','netaji']
     right='gandhi'
     response=[{"type": "postback", "title": 'gandhi',"payload": 'right'},{"type": "postback", "title": 'jayanta',"payload": 'right'},
