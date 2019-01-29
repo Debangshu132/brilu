786e2d236a3840fbd356318ecf49a334b73b6609
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-01-29 22:17:15 +0530

    Update decisionTree.py

--- decisionTree.py
+++ decisionTree.py
@@ -9,6 +9,13 @@ def fetchData():
     # p.pprint(cursor[0])
     userInfo = cursor[0]
     return(userInfo)
+def updateUsersInformation(ID, **kwargs):
+    MONGODB_URI = "mongodb://Debangshu:Starrynight.1@ds163694.mlab.com:63694/brilu"
+    client = MongoClient(MONGODB_URI, connectTimeoutMS=30000)
+    db = client.get_database("brilu")
+    for key in kwargs:
+        db.userInfo.update({"_id" : "5c4e064ffb6fc05326ad8c57"}, {"$set":{str(ID)+"."+str(key): kwargs[key]}},upsert=True);
+    return(0)
 
 
 def listOfExams():
