3977f080e4b53874718cb5409b097e211cc631f4
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-01-30 00:04:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def checkQuickReply(text,id):
                 return True
            if msges[0]=="Show Results":
                send_message(id,'a','a', msges[1])
-               result="I have asked you "+str(getUserInformation(id,'totalquestionasked'))+'until now, out of which you got '+str(getUserInformation(id,'totalquestionright'))+'correct!'
+               result="I have asked you "+str(getUserInformation(id,'totalquestionasked'))+' questions until now, out of which you got '+str(getUserInformation(id,'totalquestionright'))+' correct!'
                quickreply(id,listofitems,result)
                return True 
            for msg in range(0,len(msges)-2):
