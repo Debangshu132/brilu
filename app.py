5b99607c24397f77b8b9a4905cb45ea0b3bedc39
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-02-06 00:59:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -288,7 +288,7 @@ def sendSuperTopic(id):
                  {
                  "title":"class10",
                    "image_url":"http://2.bp.blogspot.com/_Q_ZJiaCqn38/TFIu3dkYfxI/AAAAAAAAACo/63Vuzi-IG4A/s1600/SCIENCE.png",
-                     "subtitle":"practice science problems from class 10",
+                     "subtitle":"practice science problems from class 10 and improve your concepts",
                         "default_action": {
                             "type":"web_url",
                             "url":"http://brilu.herokuapp.com/result/"+str(id),
@@ -303,7 +303,7 @@ def sendSuperTopic(id):
                   {
                  "title":"Job Preparation",
                    "image_url":"http://dubingroup.com/wp-content/uploads/2014/04/shutterstock_70237444.jpg",
-                      "subtitle":"practice problems that makes you ready for interview",
+                      "subtitle":"practice problems that makes you ready for interview I have aptitude,verbal ability and logical reasoning",
                         "default_action": {
                             "type":"web_url",
                             "url":"http://brilu.herokuapp.com/result/"+str(id),
