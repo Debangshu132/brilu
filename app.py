cc6345e56f99a39aa9d556ac656778e47f24b634
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-02-09 03:17:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ def sendQuestion(id):
     question,options,right,hint,solution,exceeded=askQuestion(getUserInformation(id,'currenttopic'))
     #options.append("hint")
     updateUsersInformation(id,lastQuestion=question,lastRightAnswer=right,lasthint=hint,lastsolution=solution)
-    bot.send_button_message(id,[{"type": "web_url","url": "https://www.google.com","title": "google baba"}])
+    bot.send_button_message(id,question,[{"type": "web_url","url": "https://www.google.com","title": "google baba"}])
     if exceeded==False:
       payload = {"recipient": {"id": id}, "message": {"quick_replies": []         
                                                      
@@ -217,28 +217,7 @@ def sendQuestion(id):
                                                       
                                                       
                                                         
-     ,   
-      "attachment":{
-        "type":"template",
-          "payload":{
-           "template_type":"generic",
-             "elements":[
-                 {
-                 "title":"a",
-                   #"image_url":https://images.pexels.com/photos/1642883/pexels-photo-1642883.jpeg?cs=srgb&dl=adults-affection-couple-1642883.jpg&fm=jpg,
-                     "subtitle":"a",
-                        "default_action": {
-                            "type":"web_url",
-                            "url":"http://brilu.herokuapp.com/result/"+str(id),
-                            "webview_height_ratio": "tall"  
-                              },
-                           "buttons":[
-                             {
-                "type":"web_url",
-                "url":"http://brilu.herokuapp.com/result/"+str(id),
-                "title":"See Details!",
-                "webview_height_ratio": "tall"  
-              }] }]}}}}
+    }}
                                                       
                                                       
                                                       
